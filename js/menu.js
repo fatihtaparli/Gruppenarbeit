cd9d96815f3a521a23b282ae943e2780877d8d1d
Anmar Aziz <anmar.aziz@stud.hs-hannover.de>
2020-11-17 15:42:40 +0100

    scroll und menu sind fertig

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,18 @@
  * Toggle the class "hidden" from the element with the id "menu", when the user clicks on the element with the id "menu-btn"
  * 
  */
+
+let menubtn = document.getElementById('menu-btn');
+let Menu = document.getElementById('menu');
+let clickedM = false;
+ 
+
+menubtn.addEventListener('click', function(){
+    if (clickedM == false) {
+        Menu.classList.remove('hidden');
+        clickedM = true;
+    }else {
+        Menu.classList.add('hidden');
+        clickedM = false;
+    }
+});
